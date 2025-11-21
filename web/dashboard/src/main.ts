1f52098d8119cb28457434878bfe850007854899
Zitronenjoghurt <117850085+Zitronenjoghurt@users.noreply.github.com>
2025-11-21 17:22:29 +0100

    add website icon

--- web/dashboard/src/main.ts
+++ web/dashboard/src/main.ts
@@ -1,7 +1,5 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
@@ -12,14 +10,14 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-    options: {
-      prefix: 'p',
-      darkModeSelector: 'system',
-      cssLayer: false,
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false,
+        },
     },
-  },
 })
 
 app.mount('#app')

13e2346307a5f23cfc020f1c5b8400e04dac1400
Zitronenjoghurt <117850085+Zitronenjoghurt@users.noreply.github.com>
2025-11-22 16:19:18 +0100

    improve overall styles

--- web/dashboard/src/components/NavBar.vue
+++ web/dashboard/src/components/NavBar.vue
@@ -1,17 +1,14 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 
-const items = ref([
-  { label: 'Home', icon: 'pi pi-home', route: '/' },
-  { label: 'About', icon: 'pi pi-info-circle', route: '/' },
-])
+const items = ref([{ label: 'Home', icon: 'pi pi-home', route: '/' }])
 </script>
 
 <template>
   <div class="mb-4">
     <Menubar :model="items" class="sticky">
       <template #start>
-        <span class="font-bold text-xl">NeoBabu</span>
+        <span class="font-bold text-xl text-primary">NeoBabu</span>
       </template>
       <template #end>
         <UserMenu />
